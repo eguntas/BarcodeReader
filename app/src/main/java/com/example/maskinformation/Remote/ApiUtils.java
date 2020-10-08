c071c8754c02db92687f23025a8197b92124df4e
eguntas <39804095+eguntas@users.noreply.github.com>
2020-10-09 00:15:15 +0300

    Update ApiUtils.java

--- app/src/main/java/com/example/maskinformation/Remote/ApiUtils.java
+++ app/src/main/java/com/example/maskinformation/Remote/ApiUtils.java
@@ -5,8 +5,7 @@ import retrofit2.Retrofit;
 public class ApiUtils {
 
     public static final String BASE_URL = "/*your web service url*/";
-    //"http://10.30.25.23:4000/"
-
+   
     public static MaskeService getMaskeService(){
         return RetrofitClient.getClient(BASE_URL).create(MaskeService.class);
     }
